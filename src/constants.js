7aad8040cafd06d6920511dfa13b42b663ea5366
retrohacker <william.jblankenship@gmail.com>
2017-05-09 13:23:44 -0700

    Merged with https://github.com/restify/plugins/pull/10

---
+++ src/constants.js
@@ -0,0 +1,32 @@
+module.exports = {
+  ALLOW_HEADERS: [
+    'accept',
+    'accept-version',
+    'content-type',
+    'request-id',
+    'origin',
+    'x-api-version',
+    'x-request-id',
+    'x-requested-with'
+  ],
+  EXPOSE_HEADERS: [
+    'api-version',
+    'content-length',
+    'content-md5',
+    'content-type',
+    'date',
+    'request-id',
+    'response-time'
+  ],
+  AC_REQ_METHOD: 'access-control-request-method',
+  AC_REQ_HEADERS: 'access-control-request-headers',
+  AC_ALLOW_CREDS: 'access-control-allow-credentials',
+  AC_ALLOW_ORIGIN: 'access-control-allow-origin',
+  AC_ALLOW_HEADERS: 'access-control-allow-headers',
+  AC_ALLOW_METHODS: 'access-control-allow-methods',
+  AC_EXPOSE_HEADERS: 'access-control-expose-headers',
+  AC_MAX_AGE: 'access-control-max-age',
+  STR_VARY: 'vary',
+  STR_ORIGIN: 'origin',
+  HTTP_NO_CONTENT: 204
+}
