9f8a136c57b087454c2624bc3ee6f9d6efa923a2
Tomasz Czechowski <tomasz@czechowski.pl>
2017-10-24 20:18:25 +0200

    Removed arrow functions + small code refactor

--- src/origin-matcher.js
+++ src/origin-matcher.js
@@ -5,20 +5,27 @@ exports.create = function (allowedOrigins) {
   // does a given request Origin match the list?
   return function (requestOrigin) {
     if (requestOrigin) {
-      return matchers.some(matcher => matcher(requestOrigin))
-    } else {
-      return false
+      return matchers.some(function (matcher) {
+        return matcher(requestOrigin)
+      })
     }
+
+    return false
   }
 }
 
 function createMatcher (allowedOrigin) {
   if (allowedOrigin.indexOf('*') === -1) {
     // simple string comparison
-    return requestOrigin => requestOrigin === allowedOrigin
-  } else {
-    // need to build a regex
-    var regex = '^' + allowedOrigin.replace('.', '\\.').replace('*', '.*') + '$'
-    return requestOrigin => requestOrigin.match(regex)
+    return function (requestOrigin) {
+      return requestOrigin === allowedOrigin
+    }
+  }
+
+  // need to build a regex
+  var regex = '^' + allowedOrigin.replace('.', '\\.').replace('*', '.*') + '$'
+
+  return function (requestOrigin) {
+    return requestOrigin.match(regex)
   }
 }
